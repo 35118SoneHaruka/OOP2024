0bdbbd2e72db6c0e2f3ff440a98759d5ddff8faf
SoneHaruka <oibc35118@ojs.ac.jp>
2024-04-24 15:55:19 +0900

    問題2.1.3完成

--- Chapter02/演習問題/Exercise1/Program.cs
+++ Chapter02/演習問題/Exercise1/Program.cs
@@ -9,6 +9,9 @@ namespace Exercise1 {
     internal class Program {
         static void Main(string[] args) {
             var songs = new Song[] {
+              new Song("let it be","the Beatles",277),
+              new Song("stand by me","Ben E King",320),
+              new Song("shake it off","Taylor Swift",242)
                 
            };
 
--- Chapter02/演習問題/Exercise1/Song.cs
+++ Chapter02/演習問題/Exercise1/Song.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace Exercise1 {
     public class Song {
-        public string Title { get; set; }
-        public string ArtistName { get; set; }
-        public int Length { get; set; }
+        private string Title { get; set; }
+        private string ArtistName { get; set; }
+        private int Length { get; set; }
 
         public Song(string title, string artistName, int length) {
             this.Title = title;
