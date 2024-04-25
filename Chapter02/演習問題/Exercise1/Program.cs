1f26c70d68b6011785c9ff1ff9a0031c0ee8e726
SoneHaruka <oibc35118@ojs.ac.jp>
2024-04-25 14:24:23 +0900

    問題2.1.4 問題2.2完成

--- Chapter02/演習問題/Exercise1/Program.cs
+++ Chapter02/演習問題/Exercise1/Program.cs
@@ -6,15 +6,26 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Exercise1 {
-    internal class Program {
+    public class Program {
         static void Main(string[] args) {
             var songs = new Song[] {
-              new Song("let it be","the Beatles",277),
+              new Song("let it be","the Beatles",243),
               new Song("stand by me","Ben E King",320),
               new Song("shake it off","Taylor Swift",242)
-                
-           };
 
+           };
+            PrintSongs(songs);
+
+        }
+
+
+
+
+        private static void PrintSongs(Song[] songs) {
+            foreach (var song in songs) {
+                Console.WriteLine(@"{0},{1},{2:mm\:ss}",song.Title,song.ArtistName, TimeSpan.FromSeconds(song.Length));
+            }
+           
         }
     }
 }
---
+++ Chapter02/演習問題/Exercise2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2 {
+    public class Program {
+        static void Main(string[] args) {
+            if (args.Length >= 1 && args[0] == "-tom") {
+                //フィートからメートルへの対応表を出力
+                PrintInchToMeterList(int.Parse(args[1]), int.Parse(args[2]));
+            } else {
+                //メートルからフィートへの対応表を出力
+                PrintMeterToInchList(int.Parse(args[1]), int.Parse(args[2]));
+            }
+
+        }
+
+        static void PrintMeterToInchList(int start, int stop) {
+            for (int meter = start; meter <= stop; meter++) {
+                double inch = InchConverter.FromMeter(meter);
+                Console.WriteLine("{0}m = {1:0.0000}in", meter, inch);
+            }
+        }
+
+        static void PrintInchToMeterList(int start, int stop) {
+            for (int inch = start; inch <= stop; inch++) {
+                double meter = InchConverter.ToMeter(inch);
+                Console.WriteLine("{0}in = {1:0.0000}m", inch, meter);
+            }
+        }
+    }
+}
