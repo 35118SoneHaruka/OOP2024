4992af729d5a12edf89ec1dcec4ebf9c192fc40f
SoneHaruka <oibc35118@ojs.ac.jp>
2024-05-15 14:10:28 +0900

    LINQ終了

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -8,14 +8,23 @@ using System.Threading.Tasks;
 namespace Section01 {
     internal class Program {
         static void Main(string[] args) {
-            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
-            //int count = numbers.Count(n => n % 2 == 0);
-            double num = numbers.Where(n => n > 5).Average();
-            int total = numbers.Where(n => n >5).Sum();
-            Console.WriteLine(num);
-            Console.WriteLine(total);
+            var names = new List<string> {
+                "Tokyo",
+                "New Delhi",
+                "Bangkok",
+                "London",
+                "Paris",
+                "Berlin",
+                "Canberra",
+                "Hong Kong",
+            };
+            IEnumerable<string> query = names.Where(s => s.Contains(' ')).Select(s => s.ToLower());
+            foreach(string s in query) {
+                Console.WriteLine(s);
+            }
+            
         }
 
-        
+
     }
 }
