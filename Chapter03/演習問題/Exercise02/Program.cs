3b7bc9de712191b4ece207ee58f9b33ed2e78856
SoneHaruka <oibc35118@ojs.ac.jp>
2024-05-22 09:57:12 +0900

    問題3.2.1完成

--- Chapter03/演習問題/Exercise02/Program.cs
+++ Chapter03/演習問題/Exercise02/Program.cs
@@ -10,39 +10,53 @@ namespace Exercise02 {
             var names = new List<string> {
                 "Tokyo",
                 "New Delhi",
-                "Bangkok", 
+                "Bangkok",
                 "London",
                 "Paris",
                 "Berlin",
                 "Canberra",
                 "Hong Kong",
             };
+
+            Console.WriteLine("***3.2.1***");
             Exercise2_1(names);
-            Console.WriteLine();
 
+            Console.WriteLine("***3.2.2***");
             Exercise2_2(names);
-            Console.WriteLine();
 
+            Console.WriteLine("***3.2.3***");
             Exercise2_3(names);
-            Console.WriteLine();
 
+            Console.WriteLine("***3.2.4***");
             Exercise2_4(names);
+
         }
 
         private static void Exercise2_1(List<string> names) {
-           
+            Console.WriteLine("都市名を入力、空行で終了");
+            do {
+                var line = Console.ReadLine();
+                if (string.IsNullOrEmpty(line)) {
+                    break;
+                } else {
+                    int index = names.FindIndex(s => s == line);
+                    Console.WriteLine(index);
+                    Console.WriteLine("都市名を入力、空行で終了");
+                }
+
+            } while (true);
         }
 
         private static void Exercise2_2(List<string> names) {
-           
+
         }
 
         private static void Exercise2_3(List<string> names) {
-           
+
         }
 
         private static void Exercise2_4(List<string> names) {
-           
+
         }
     }
 }
