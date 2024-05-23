0e7247f2135e2fa8d9469fa2dc88f7d367244907
SoneHaruka <oibc35118@ojs.ac.jp>
2024-05-23 13:36:15 +0900

    BallGame完成

--- FormApp/BallApp/SoccerBall.cs
+++ FormApp/BallApp/SoccerBall.cs
@@ -2,39 +2,64 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace BallApp {
     internal class SoccerBall : Obj {
-        Random rand = new Random();
-        public static int Count { get; set; }
-        public SoccerBall(double xp,double yp) 
-           :base(xp -25, yp -25, @"Picture\soccer_ball.png"){
+        Random random = new Random();   //乱数インスタンス
 
-            MoveX = rand.Next(-25,25);//移動量設定
-            MoveY = rand.Next(-25, 25);
+        public static int Count {  get; set; }
+
+        public SoccerBall(double xp, double yp)
+            : base(xp, yp, @"Picture\soccer_ball.png") {
+#if DEBUG
+            MoveX = 5;
+            MoveY = 5;
+#else
+            MoveX = random.Next(-15, 15); //移動量設定
+            MoveY = random.Next(-15, 15); ;
+#endif
+
+
             Count++;
         }
 
-        public override bool Move(PictureBox pbBar, PictureBox pbBall) {
+        public override int Move(PictureBox pbBar, PictureBox pbBall) {
+            int ret = 0;
+            Rectangle rBar = new Rectangle(pbBar.Location.X,pbBar.Location.Y,
+                                                         pbBar.Width, pbBar.Height);
 
-            Rectangle rBar = new Rectangle(pbBar.Location.X, pbBar.Location.Y, pbBar.Width, pbBar.Height);
-            Rectangle rBall = new Rectangle(pbBall.Location.X, pbBall.Location.Y, pbBall.Width, pbBall.Height);
-            if (PosX>750 || PosX < 0) {
+            Rectangle rBall = new Rectangle(pbBall.Location.X, pbBall.Location.Y,
+                                                         pbBall.Width, pbBall.Height);
+
+
+            if (PosX > 750 || PosX < 0) {
+                //移動量の符号を反転
                 MoveX = -MoveX;
             }
 
-            if(PosY > 500 || PosY < 0 || rBar.IntersectsWith(rBall)) { 
+            if (PosY < 0) {
+                //移動量の符号を反転
                 MoveY = -MoveY;
+
             }
+            if (rBar.IntersectsWith(rBall)) {
+                MoveY = -MoveY;
+                ret = 2;
+            }
+
 
 
             PosX += MoveX;
             PosY += MoveY;
-           
-           
 
-            return true;
+            //下に落下したか？
+            if (PosY > 600)
+                ret = 1;
+
+
+            return ret;
         }
 
         public override bool Move(Keys direction) {
--- FormApp/BallApp/TennisBall.cs
+++ FormApp/BallApp/TennisBall.cs
@@ -7,36 +7,59 @@ using System.Threading.Tasks;
 
 namespace BallApp {
     internal class TennisBall : Obj {
-        Random rand = new Random();
-        public static int Count { get; set; }  
+        Random random = new Random();   //乱数インスタンス
+
+        public static int Count { get; set; }
+
         public TennisBall(double xp, double yp)
-        : base(xp -15, yp -15, @"Picture\tennis_ball.png"){
-            
-            MoveX = rand.Next(-25, 25);//移動量設定
-            MoveY = rand.Next(-25, 25);
+            : base(xp, yp, @"Picture\tennis_ball.png") {
+#if DEBUG
+            MoveX = 5;
+            MoveY = 5;
+#else
+            MoveX = random.Next(-15, 15); //移動量設定
+            MoveY = random.Next(-15, 15); ;
+#endif
+
+
+
             Count++;
         }
+        //戻り値：０…移動OK　１…落下　２…バーに当たった
+        public override int Move(PictureBox pbBar, PictureBox pbBall) {
+            int ret = 0;
+            Rectangle rBar = new Rectangle(pbBar.Location.X, pbBar.Location.Y,
+                                                         pbBar.Width, pbBar.Height);
 
-    public override bool Move(PictureBox pbBar,PictureBox pbBall) {
-            Rectangle rBar = new Rectangle(pbBar.Location.X,pbBar.Location.Y,pbBar.Width,pbBar.Height);
-            Rectangle rBall = new Rectangle(pbBall.Location.X, pbBall.Location.Y, pbBall.Width, pbBall.Height);
-
+            Rectangle rBall = new Rectangle(pbBall.Location.X, pbBall.Location.Y,
+                                                         pbBall.Width, pbBall.Height);
             if (PosX > 750 || PosX < 0) {
-            MoveX = -MoveX;
+                //移動量の符号を反転
+                MoveX = -MoveX;
+            }
+
+            if (PosY < 0) {
+                //移動量の符号を反転
+                MoveY = -MoveY;
+                
+            }
+            if (rBar.IntersectsWith(rBall)) {
+                MoveY = -MoveY;
+                ret= 2;
+            }
+
+
+
+            PosX += MoveX;
+            PosY += MoveY;
+
+            //下に落下したか？
+            if (PosY > 600)
+                ret = 1;
+                
+
+            return ret;
         }
-
-        if (PosY > 540 || PosY < 0 || rBar.IntersectsWith(rBall)) {
-            MoveY = -MoveY;
-        }
-
-
-        PosX += MoveX;
-        PosY += MoveY;
-
-       
-
-        return true;
-    }
 
         public override bool Move(Keys direction) {
             return true;
