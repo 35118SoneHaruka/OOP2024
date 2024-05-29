01fa40c25404f7349617714da45450831c2f8aa7
SoneHaruka <oibc35118@ojs.ac.jp>
2024-05-29 15:40:42 +0900

    問題5.2 完成

--- Chapter05/演習問題/Exercise02/Program.cs
+++ Chapter05/演習問題/Exercise02/Program.cs
@@ -7,6 +7,12 @@ using System.Threading.Tasks;
 namespace Exercise02 {
     internal class Program {
         static void Main(string[] args) {
+            Console.Write("数字を入力してください：");
+            var str = Console.ReadLine();
+            int.TryParse(str, out var num);
+            var s1 = num.ToString("#,0");
+            Console.Write("カンマ付きに変換：");
+            Console.WriteLine(s1);
         }
     }
 }
