d9c5eb8707637c606ae4942a439f721b4c972569
SoneHaruka <oibc35118@ojs.ac.jp>
2024-05-29 13:57:03 +0900

    問題2.3完成

--- Chapter04/演習問題/Exercise02/Program.cs
+++ Chapter04/演習問題/Exercise02/Program.cs
@@ -12,7 +12,7 @@ namespace Exercise02 {
             var ymCollection = new YearMonth[] {
                 new YearMonth(1980, 1),
                 new YearMonth(1990, 4),
-                new YearMonth(2000, 7),
+                new YearMonth(2004, 6),
                 new YearMonth(2010, 9),
                 new YearMonth(2020, 12),
             };
@@ -37,8 +37,23 @@ namespace Exercise02 {
             }
         }
 
+        private static YearMonth Exercise2_3(YearMonth[] ymCollection) {
+            foreach (var item in ymCollection) {
+                if (item.Is21Century) {
+                    return item;
+                }
+
+            }
+            return null;
+        }
+
         private static void Exercise2_4(YearMonth[] ymCollection) {
-            
+            if(Exercise2_3(ymCollection)== null) {
+                Console.WriteLine("21世紀のデータはありません");
+            } else {
+                Console.WriteLine(Exercise2_3(ymCollection) );
+            }
+
         }
 
         private static void Exercise2_5(YearMonth[] ymCollection) {
