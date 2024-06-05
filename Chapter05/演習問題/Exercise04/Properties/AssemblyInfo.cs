61fa34649359715a979fa3b9d7018b908d2dae50
SoneHaruka <oibc35118@ojs.ac.jp>
2024-06-05 09:35:37 +0900

    演習問題5.3完成

--- Chapter05/演習問題/Exercise04/Properties/AssemblyInfo.cs
+++ Chapter05/演習問題/Exercise04/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般的な情報は、次の方法で制御されます。
 // 制御されます。アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更します。
-[assembly: AssemblyTitle("Exercise03")]
+[assembly: AssemblyTitle("Exercise04")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Exercise03")]
+[assembly: AssemblyProduct("Exercise04")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
-[assembly: Guid("3b6d1f8b-673f-4718-9cd4-34a9315de937")]
+[assembly: Guid("7a0f6d94-e2d2-45dc-b3ad-28fe49c28cac")]
 
 // アセンブリのバージョン情報は次の 4 つの値で構成されています:
 //
