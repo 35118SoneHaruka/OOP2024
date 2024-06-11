f8ac8592b491dd08dae8647cee3da5b99dcb7833
SoneHaruka <oibc35118@ojs.ac.jp>
2024-06-11 15:09:13 +0900

    都道府県検索プログラム　完成

--- Chapter07/Section01/Program.cs
+++ Chapter07/Section01/Program.cs
@@ -13,9 +13,23 @@ namespace Section01 {
                 Console.WriteLine("県庁所在地の登録" + i);
                 Console.Write("都道府県：");
                 var prefecture = Console.ReadLine();
-                Console.Write("県庁所在地：");
-                var prefecturalOffice = Console.ReadLine();
-                prefectureDict.Add(prefecture, prefecturalOffice);
+                string prefecturalOffice;
+                if (prefectureDict.ContainsKey(prefecture)) {
+                    Console.WriteLine("上書きしますか");
+                    Console.WriteLine("1:yes");
+                    Console.WriteLine("2:no");
+                    if (int.Parse(Console.ReadLine()) == 1) {
+                        Console.Write("県庁所在地：");
+                        prefecturalOffice = Console.ReadLine();
+                        prefectureDict[prefecture] = prefecturalOffice;
+                    }
+
+                } else {
+                    Console.Write("県庁所在地：");
+                    prefecturalOffice = Console.ReadLine();
+                    prefectureDict.Add(prefecture, prefecturalOffice);
+                }
+
 
             }
 
@@ -37,8 +51,11 @@ namespace Section01 {
                     }
                 }else if(select == 2) {
                     Console.Write("都道府県：");
-                    var prefecture1 = prefectureDict[Console.ReadLine()];
-                    Console.WriteLine("県庁所在地:" + prefecture1);
+                    var prefecture1 = Console.ReadLine();
+                    if (prefectureDict.ContainsKey(prefecture1)) {
+                        Console.WriteLine("県庁所在地:" + prefectureDict[prefecture1]);
+                    }
+                    
                 }
                 Console.WriteLine("*メニュー*");
                 Console.WriteLine("1:一覧表示");
@@ -50,61 +67,6 @@ namespace Section01 {
 
             }
             Console.WriteLine("終了します");
-
-
-
-            /*foreach (var item in prefectureDict) {
-                Console.WriteLine("{0}の県庁所在地は{1}です", item.Key, item.Value);
-              }*/
-
-
-
-
-
-            /*switch (select) {
-                case 1:
-                    foreach (var prefecture in prefectureDict) {
-                        Console.WriteLine("{0}の県庁所在地は{1}です", prefecture.Key, prefecture.Value);
-                    }
-                    break;
-                case 2:
-                    Console.Write("都道府県：");
-                    var prefecture1 = prefectureDict[Console.ReadLine()];
-                    Console.WriteLine("県庁所在地:" + prefecture1);
-                    break;
-                case 9:
-                    Console.WriteLine("終了します");
-                    break;
-            }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            /*var employeeDict = new Dictionary<int, Employee> {
-               { 100, new Employee(100, "清水遼久") },
-               { 112, new Employee(112, "芹沢洋和") },
-               { 125, new Employee(125, "岩瀬奈央子") },
-            };
-
-            employeeDict.Add(126, new Employee(126, "曽根遼"));
-
-            var name = employeeDict.Where(e => e.Value.Name.Contains("和"));
-
-            
-            foreach (var item in employeeDict.Values) {
-                Console.WriteLine($"{item.Id}");
-            }*/
         }
     }
 }
