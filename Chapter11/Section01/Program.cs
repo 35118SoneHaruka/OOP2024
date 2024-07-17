57ef3d3a594619bdfcd03e2e09fb7dbd1e705266
SoneHaruka <oibc35118@ojs.ac.jp>
2024-07-17 14:57:10 +0900

    教科書ｐ283まで

---
+++ Chapter11/Section01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var xdoc = XDocument.Load("novelists.xml");
+            var xelements = xdoc.Root.Elements();
+            foreach (var xnovelist in xelements) {
+                var xname = xnovelist.Element("name");//要素の取得
+                var xwork = xnovelist.Element("masterpieces").Elements("title").Select(x => x.Value);
+
+                Console.WriteLine("{0} - {1}", xname.Value, string.Join(",", xwork));
+            }
+        }
+    }
+}
