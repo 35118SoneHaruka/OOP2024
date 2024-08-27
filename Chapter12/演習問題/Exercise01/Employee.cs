40da89880085683e08a1e6b53ce912dbe240e73f
SoneHaruka <oibc35118@ojs.ac.jp>
2024-08-27 11:09:08 +0900

    問題12.1.1完成

---
+++ Chapter12/演習問題/Exercise01/Employee.cs
@@ -0,0 +1,7 @@
+using System;
+
+public class Employee {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public DateTime HireDate { get; set; }
+}
---
+++ Chapter12/演習問題/Exercise01/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            Exercise1_1("employee.xml");
+
+            // これは確認用
+            Console.WriteLine(File.ReadAllText("employee.xml"));
+            Console.WriteLine();
+
+            Exercise1_2("employees.xml");
+            Exercise1_3("employees.xml");
+            Console.WriteLine();
+
+            Exercise1_4("employees.json");
+
+            // これは確認用
+            Console.WriteLine(File.ReadAllText("employees.json"));
+        }
+
+        private static void Exercise1_1(string outfile) {
+            var employee = new Employee {
+                Id = 001,
+                Name = "sizen",
+                HireDate = new DateTime(2004, 6, 7),
+            };
+
+            using (var writer = XmlWriter.Create(outfile)) {
+                var serializer = new XmlSerializer(employee.GetType());
+                serializer.Serialize(writer, employee);
+            }
+
+            using (var reader = XmlReader.Create(outfile)) {
+                var serializer = new XmlSerializer(typeof(Employee));
+                var emp = serializer.Deserialize(reader);
+                Console.WriteLine(emp);
+            }
+        }
+
+        private static void Exercise1_2(string putfile) {
+            
+        }
+
+        private static void Exercise1_3(string outfile) {
+            
+        }
+
+        private static void Exercise1_4(string outfile) {
+            
+        }
+    }
+}
