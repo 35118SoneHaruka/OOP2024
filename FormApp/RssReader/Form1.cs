9e16c769d6d36d19d9a2872cf87c3365a9578ae7
SoneHaruka <oibc35118@ojs.ac.jp>
2024-09-03 11:13:12 +0900

    xmlからリストボックスにタイトル表示

---
+++ FormApp/RssReader/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace RssReader {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btGet_Click(object sender, EventArgs e) {
+            using (var wc = new WebClient()) {
+                var url = wc.OpenRead(tbRssUrl.Text);
+                var xdoc = XDocument.Load(url);
+                var xdocs = xdoc.Root.Descendants("item");
+                foreach (var xdocument in xdocs) {
+                    var works = xdocument.Element("title").Value;
+                    lbRssTitle.Items.Add(works.ToString());
+                }
+            }
+        }
+    }
+}
