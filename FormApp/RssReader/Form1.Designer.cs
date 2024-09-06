a3b12261171f564b5ebf2d830a77e8d2c31db7b3
SoneHaruka <oibc35118@ojs.ac.jp>
2024-09-06 10:15:28 +0900

    webview2を使ってwebサイトを表示

--- FormApp/RssReader/Form1.Designer.cs
+++ FormApp/RssReader/Form1.Designer.cs
@@ -26,19 +26,20 @@
             this.tbRssUrl = new System.Windows.Forms.TextBox();
             this.btGet = new System.Windows.Forms.Button();
             this.lbRssTitle = new System.Windows.Forms.ListBox();
-            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
             this.SuspendLayout();
             // 
             // tbRssUrl
             // 
             this.tbRssUrl.Location = new System.Drawing.Point(33, 27);
             this.tbRssUrl.Name = "tbRssUrl";
-            this.tbRssUrl.Size = new System.Drawing.Size(536, 19);
+            this.tbRssUrl.Size = new System.Drawing.Size(674, 19);
             this.tbRssUrl.TabIndex = 0;
             // 
             // btGet
             // 
-            this.btGet.Location = new System.Drawing.Point(599, 25);
+            this.btGet.Location = new System.Drawing.Point(713, 25);
             this.btGet.Name = "btGet";
             this.btGet.Size = new System.Drawing.Size(75, 23);
             this.btGet.TabIndex = 1;
@@ -52,28 +53,33 @@
             this.lbRssTitle.ItemHeight = 12;
             this.lbRssTitle.Location = new System.Drawing.Point(33, 61);
             this.lbRssTitle.Name = "lbRssTitle";
-            this.lbRssTitle.Size = new System.Drawing.Size(536, 100);
+            this.lbRssTitle.Size = new System.Drawing.Size(181, 532);
             this.lbRssTitle.TabIndex = 2;
+            this.lbRssTitle.SelectedIndexChanged += new System.EventHandler(this.lbRssTitle_SelectedIndexChanged);
             // 
-            // webBrowser1
+            // webView21
             // 
-            this.webBrowser1.Location = new System.Drawing.Point(33, 167);
-            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
-            this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.Size = new System.Drawing.Size(536, 269);
-            this.webBrowser1.TabIndex = 3;
+            this.webView21.AllowExternalDrop = true;
+            this.webView21.CreationProperties = null;
+            this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView21.Location = new System.Drawing.Point(234, 61);
+            this.webView21.Name = "webView21";
+            this.webView21.Size = new System.Drawing.Size(554, 532);
+            this.webView21.TabIndex = 3;
+            this.webView21.ZoomFactor = 1D;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.webBrowser1);
+            this.ClientSize = new System.Drawing.Size(800, 597);
+            this.Controls.Add(this.webView21);
             this.Controls.Add(this.lbRssTitle);
             this.Controls.Add(this.btGet);
             this.Controls.Add(this.tbRssUrl);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -84,7 +90,7 @@
         private System.Windows.Forms.TextBox tbRssUrl;
         private System.Windows.Forms.Button btGet;
         private System.Windows.Forms.ListBox lbRssTitle;
-        private System.Windows.Forms.WebBrowser webBrowser1;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
     }
 }
 
--- FormApp/RssReader/Form1.cs
+++ FormApp/RssReader/Form1.cs
@@ -3,29 +3,44 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO.Pipes;
 using System.Linq;
 using System.Net;
+using System.Runtime.Remoting.Channels;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml.Linq;
 
 namespace RssReader {
+   
+
     public partial class Form1 : Form {
+        private List<ItemData> xdocs;
         public Form1() {
             InitializeComponent();
         }
 
         private void btGet_Click(object sender, EventArgs e) {
+
             using (var wc = new WebClient()) {
                 var url = wc.OpenRead(tbRssUrl.Text);
                 var xdoc = XDocument.Load(url);
-                var xdocs = xdoc.Root.Descendants("item");
+                xdocs = xdoc.Root.Descendants("item").Select(item => new ItemData { Title = item.Element("title").Value, link = item.Element("link").Value }).ToList();
                 foreach (var xdocument in xdocs) {
-                    var works = xdocument.Element("title").Value;
-                    lbRssTitle.Items.Add(works.ToString());
+                    lbRssTitle.Items.Add(xdocument.Title);
                 }
             }
         }
+
+        private void lbRssTitle_SelectedIndexChanged(object sender, EventArgs e) {
+            Uri websiteUri = new Uri(xdocs[lbRssTitle.SelectedIndex].link);
+            webView21.Source = websiteUri;
+            //webView21.CoreWebView2.Navigate(xdocs[lbRssTitle.SelectedIndex].link);
+        }
+    }
+    public class ItemData {
+        public string Title { get; set; }
+        public string link { get; set; }
     }
 }
