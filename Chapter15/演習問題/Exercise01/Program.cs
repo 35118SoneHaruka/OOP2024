3292318c30efc47055da2ba09d1d2471fb1ab3cf
SoneHaruka <oibc35118@ojs.ac.jp>
2024-09-30 15:07:28 +0900

    問題１５.１.4終了

---
+++ Chapter15/演習問題/Exercise01/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            Exercise1_2();
+            Console.WriteLine();
+            Exercise1_3();
+            Console.WriteLine();
+            Exercise1_4();
+            Console.WriteLine();
+            Exercise1_5();
+            Console.WriteLine();
+            Exercise1_6();
+            Console.WriteLine();
+            Exercise1_7();
+            Console.WriteLine();
+            Exercise1_8();
+
+            Console.ReadLine();
+        }
+
+        private static void Exercise1_2() {
+            var price = Library.Books.Max(x => x.Price);
+            var book = Library.Books.First(b => b.Price == price);
+            Console.WriteLine(book);
+        }
+
+        private static void Exercise1_3() {
+            var groups = Library.Books.GroupBy(b => b.PublishedYear).Select(g => new { PublishedYear = g.Key, Count = g.Count() }).OrderBy(o => o.PublishedYear);
+            foreach (var group in groups) {
+                Console.WriteLine($"{group.PublishedYear} {group.Count}冊");
+            };
+        }
+
+        private static void Exercise1_4() {
+            var books = Library.Books.OrderByDescending(b => b.PublishedYear).ThenByDescending(b => b.Price);
+            foreach (var book in books) {
+                Console.WriteLine($"{book.PublishedYear}年  {book.Price}円　{book.Title}");
+            }
+        }
+
+        private static void Exercise1_5() {
+           
+        }
+
+        private static void Exercise1_6() {
+           
+        }
+
+        private static void Exercise1_7() {
+            
+        }
+
+        private static void Exercise1_8() {
+            
+        }
+    }
+}
