ab98f473a70c78423eefa7995e4b95f3d59c2573
SoneHaruka <oibc35118@ojs.ac.jp>
2024-10-09 14:56:48 +0900

    第１７章

---
+++ Chapter17/TemplateMethod/TextFileProcessor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace TextFileProcessor {
+    internal class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
