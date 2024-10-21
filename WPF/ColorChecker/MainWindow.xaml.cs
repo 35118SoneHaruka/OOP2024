69c220219976e4f3f1b08766be6ee92af4a34efe
SoneHaruka <oibc35118@ojs.ac.jp>
2024-10-21 16:00:50 +0900

    backColor変更

--- WPF/ColorChecker/MainWindow.xaml.cs
+++ WPF/ColorChecker/MainWindow.xaml.cs
@@ -20,6 +20,23 @@ namespace ColorChecker {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+            rValue.Text = "0";
+            gValue.Text = "0";
+            bValue.Text = "0";
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
+            int redValue = (int)rSlider.Value;
+            rValue.Text = redValue.ToString();
+            int greenValue = (int)gSlider.Value;
+            gValue.Text = greenValue.ToString();
+            int blueValue = (int)bSlider.Value;
+            bValue.Text = blueValue.ToString();
+            int alpha = 255;
+
+            colorArea.Background = new SolidColorBrush(Color.FromArgb((byte)alpha, (byte)redValue, (byte)greenValue, (byte)blueValue));
+
+            
         }
     }
 }
