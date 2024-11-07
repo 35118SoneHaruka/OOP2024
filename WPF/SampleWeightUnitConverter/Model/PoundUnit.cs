0b3f94f7c2d6cd3250c73ef52266af9683ee5272
SoneHaruka <oibc35118@ojs.ac.jp>
2024-11-07 11:01:31 +0900

    stone,t追加

--- WPF/SampleWeightUnitConverter/Model/PoundUnit.cs
+++ WPF/SampleWeightUnitConverter/Model/PoundUnit.cs
@@ -7,8 +7,9 @@ using System.Threading.Tasks;
 namespace SampleWeightUnitConverter {
     public class PoundUnit : WeightUnit {
         private static List<PoundUnit> units = new List<PoundUnit> {
-            new PoundUnit{Name ="lb" ,Coefficient = 16},
             new PoundUnit{Name ="oz" ,Coefficient = 1},
+            new PoundUnit{Name ="lb" ,Coefficient = 16},
+            new PoundUnit{Name ="stone" ,Coefficient = 224},
         };
         public static ICollection<PoundUnit> Units { get { return units; } }
 
