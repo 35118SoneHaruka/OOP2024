a2073b6889f10c8550ce1f419f7dc2ab08d49085
SoneHaruka <oibc35118@ojs.ac.jp>
2024-11-14 12:08:28 +0900

    起動時にデータベース読み込み

--- SQLite/CustomerApp/MainWindow.xaml.cs
+++ SQLite/CustomerApp/MainWindow.xaml.cs
@@ -20,8 +20,10 @@ namespace CustomerApp {
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
     public partial class MainWindow : Window {
+        List<Customer> _customers;
         public MainWindow() {
             InitializeComponent();
+            ReadDatabase();
         }
 
         private void SaveButton_Click(object sender, RoutedEventArgs e) {
@@ -35,15 +37,39 @@ namespace CustomerApp {
                 connection.CreateTable<Customer>();
                 connection.Insert(customer);
             }
+            ReadDatabase();
         }
 
         private void ReadButton_Click(object sender, RoutedEventArgs e) {
+            ReadDatabase();
+        }
+
+        private void ReadDatabase() {
+            using (var connection = new SQLiteConnection(App.databasePass)) {
+                connection.CreateTable<Customer>();
+                _customers = connection.Table<Customer>().ToList();
+
+                CustomerListView.ItemsSource = _customers;
+            }
+        }
+
+        private void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e) {
+            var filterList = _customers.Where(x => x.Name.Contains(SearchTextBox.Text)).ToList();
+            CustomerListView.ItemsSource = filterList;
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e) {
+            var item = CustomerListView.SelectedItem as Customer;
+            if(item == null) {
+                MessageBox.Show("削除する行を選択してください");
+                return;
+            }
 
             using (var connection = new SQLiteConnection(App.databasePass)) {
                 connection.CreateTable<Customer>();
-                var customers = connection.Table<Customer>().ToList();
+                connection.Delete(item);
 
-                CustomerListView.ItemsSource = customers;
+                ReadDatabase();
             }
         }
     }
